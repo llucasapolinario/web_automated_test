bddfa85f2d536f2b9f42a5306a2a4a29f1ec62cc
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-01 08:03:26 -0300

    create to put all constants ID, url and text

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,16 @@
+package utils;
+
+public abstract class Constants {
+
+    public static final String BASEURL = "http://mantis.fernando.base2.com.br/";
+
+    // LOGIN
+    public static final String USERNAME_ID = "administrator";
+    public static final String PASSWORD_ID = "qwe";
+
+    public static final String LOGINBUTTON_ID = "loginButton";
+
+    public static final String errorMessageUsernameXpath = "//*[@id=\"loginForm\"]/div[1]/div/div";
+    public static final String errorMessagePasswordXpath = "//*[@id=\"loginForm\"]/div[2]/div/div ";
+
+}
