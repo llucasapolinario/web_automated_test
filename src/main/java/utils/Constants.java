ce003c803c2d36440e41e76ed43a0f862fe6836b
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-10 13:06:51 -0300

    clean

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -11,7 +11,7 @@ public abstract class Constants {
     public static final String PASSWORD_ID = "password";
     public static final String LOGIN_XPATH = "//input[@value='Entrar']";
 
-    public static final String errorMessagePasswordXpath = "//div[@id='main-container']/div/div/div/div/div[4]/p";
+    public static final String ERROR_MESSAGE_PASSWORD_XPATH = "//div[@id='main-container']/div/div/div/div/div[4]/p";
     public static final String MESSAGE_FAILURE_LOGIN = "Sua conta pode estar desativada ou bloqueada ou " +
             "o nome de usuário e a senha que você digitou não estão corretos.";
 
