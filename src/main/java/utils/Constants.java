93ad1ae414f6509030e83489a480b3872f08fa93
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-10 13:01:39 -0300

    add screenshot_folder and clean

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,8 +1,10 @@
 package utils;
 
+import java.io.File;
+
 public abstract class Constants {
 
-    public static final String BASEURL = "http://mantis.fernando.base2.com.br/";
+    public static final String BASE_URL = "http://mantis.fernando.base2.com.br/";
 
     // LOGIN
     public static final String USERNAME_ID = "username";
@@ -10,7 +12,11 @@ public abstract class Constants {
     public static final String LOGIN_XPATH = "//input[@value='Entrar']";
 
     public static final String errorMessagePasswordXpath = "//div[@id='main-container']/div/div/div/div/div[4]/p";
-    public static final String MESSAGE_FAILURE_LOGIN = "Sua conta pode estar desativada ou bloqueada ou o nome de usuário e a senha que você digitou não estão corretos.";
+    public static final String MESSAGE_FAILURE_LOGIN = "Sua conta pode estar desativada ou bloqueada ou " +
+            "o nome de usuário e a senha que você digitou não estão corretos.";
 
+    // FOLDER
+    static final String SCREENSHOT_FOLDER = System.getProperty("user.dir") + File.separator +
+            "ScreenShots" + File.separator;
 
 }
