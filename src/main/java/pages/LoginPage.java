94f2721633b168174a232592d78757f8dce6b0bc
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-11 11:53:28 -0300

    add create user link

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -11,6 +11,7 @@ public class LoginPage extends BasePage {
     private static final String USERNAME_ID = "username";
     private static final String PASSWORD_ID = "password";
     private static final String ERROR_MESSAGE_PASSWORD_XPATH = "//div[@id='main-container']/div/div/div/div/div[4]/p";
+    private static final String CREATE_NEW_USER_LINK = "criar uma nova conta";
 
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -33,6 +34,10 @@ public class LoginPage extends BasePage {
 
     public void clickLogin() {
         click(By.xpath(LOGIN_XPATH));
+    }
+
+    public void clickCreateUser() {
+        click(By.linkText(CREATE_NEW_USER_LINK));
     }
 
     public boolean isLoginFail(String expectedText) {
