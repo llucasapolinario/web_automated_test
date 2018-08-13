87ecd185953f3c4b371b597ba4bb0cb968864eef
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-13 13:05:25 -0300

    create ExcelDataDriven class

---
+++ src/main/java/utils/ExcelDataDriven.java
@@ -0,0 +1,115 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.Platform;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static utils.Constants.USER_DIR;
+import static utils.Utils.getCurrentPlatform;
+
+public class ExcelDataDriven {
+
+    private static final String testDataExcelFileName = "";
+    public static String testDataExcelPath = null;
+    public static int rowNumber;
+    public static int columnNumber;
+    private static XSSFWorkbook excelWBook;
+    private static XSSFSheet excelWSheet;
+    private static XSSFCell cell;
+    private static XSSFRow row;
+
+    public static int getRowNumber() {
+        return rowNumber;
+    }
+
+    public static void setRowNumber(int pRowNumber) {
+        rowNumber = pRowNumber;
+    }
+
+    public static int getColumnNumber() {
+        return columnNumber;
+    }
+
+    public static void setColumnNumber(int pColumnNumber) {
+        columnNumber = pColumnNumber;
+    }
+
+    // This method has two parameters: "Test data excel file name" and "Excel sheet name"
+    // It creates FileInputStream and set excel file and excel sheet to excelWBook and excelWSheet variables.
+    public static void setExcelFileSheet(String sheetName) {
+        //MAC or Windows Selection for excel path
+        if (getCurrentPlatform().equals(Platform.MAC)) {
+            testDataExcelPath = USER_DIR + "//src//test//java//resources//";
+        } else if (getCurrentPlatform().equals(Platform.WINDOWS)) {
+            testDataExcelPath = USER_DIR + "\\src\\test\\java\\resources\\";
+        }
+        try {
+            // Open the Excel file
+            FileInputStream ExcelFile = new FileInputStream(testDataExcelPath + testDataExcelFileName);
+            excelWBook = new XSSFWorkbook(ExcelFile);
+            excelWSheet = excelWBook.getSheet(sheetName);
+        } catch (Exception e) {
+            try {
+                throw (e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    //This method reads the test data from the Excel cell.
+    //We are passing row number and column number as parameters.
+    public static String getCellData(int RowNum, int ColNum) {
+        try {
+            cell = excelWSheet.getRow(RowNum).getCell(ColNum);
+            DataFormatter formatter = new DataFormatter();
+            String cellData = formatter.formatCellValue(cell);
+            return cellData;
+        } catch (Exception e) {
+            throw (e);
+        }
+    }
+
+    //This method takes row number as a parameter and returns the data of given row number.
+    public static XSSFRow getRowData(int RowNum) {
+        try {
+            row = excelWSheet.getRow(RowNum);
+            return row;
+        } catch (Exception e) {
+            throw (e);
+        }
+    }
+
+    //This method gets excel file, row and column number and set a value to the that cell.
+    public static void setCellData(String value, int RowNum, int ColNum) {
+        try {
+            row = excelWSheet.getRow(RowNum);
+            cell = row.getCell(ColNum);
+            if (cell == null) {
+                cell = row.createCell(ColNum);
+                cell.setCellValue(value);
+            } else {
+                cell.setCellValue(value);
+            }
+            // Constant variables Test Data path and Test Data file name
+            FileOutputStream fileOut = new FileOutputStream(testDataExcelPath + testDataExcelFileName);
+            excelWBook.write(fileOut);
+            fileOut.flush();
+            fileOut.close();
+        } catch (Exception e) {
+            try {
+                throw (e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+}
