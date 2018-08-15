db096f237273e0bf304d7325414457c8c8c43094
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-14 21:37:57 -0300

    fix singleton Webdriver

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -1,38 +1,44 @@
 package utils;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-import java.util.Set;
 
 public class Driver {
-
-
+    
     private static WebDriver driver;
     private static WebDriverWait wait;
-    private static final int TIME_OUT = 1500;
 
     private static final Object lock = new Object();
 
-    public static WebDriver getDriverInstance() {
-        if (driver == null) {
+    public static void newInstance() {
         synchronized (lock) {
             driver = new ChromeDriver();
+            wait = new WebDriverWait(Driver.getDriverInstance(),
+                    PropertyManager.getInstance().getTimeOut());
         }
     }
-        return driver;
-}
 
-    public static WebDriverWait getWaitInstance(){
-        if (wait == null) {
+    public static WebDriver getDriverInstance() {
+        if (driver == null) {
             synchronized (lock) {
-                wait = new WebDriverWait(Driver.getDriverInstance(), TIME_OUT);
+                driver = new ChromeDriver();
             }
         }
+
+        return driver;
+    }
+
+    public static WebDriverWait getWaitInstance() {
+        if (wait == null) {
+            synchronized (lock) {
+                wait = new WebDriverWait(Driver.getDriverInstance(),
+                        PropertyManager.getInstance().getTimeOut());
+            }
+        }
+
         return wait;
     }
+
 }
