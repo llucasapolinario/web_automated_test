6c8a81512f3680ce12f787b8835b14fb203a77de
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-26 16:31:24 -0300

    refactor after extent report

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -5,14 +5,20 @@ import java.io.File;
 public abstract class Constants {
 
     public static final Object LOCK = new Object();
+
     // FOLDER
     static final String USER_DIR = "user.dir";
-    static final String SCREENSHOT_FOLDER = System.getProperty(USER_DIR) + File.separator +
-            "ScreenShots" + File.separator;
+    private static final String FS = File.separator;
 
+    static final String SCREEN_SHOT_FOLDER =
+            System.getProperty(USER_DIR) + FS + "ScreenShots" + FS;
+    public static final String ExtentConfigPath = System.getProperty(USER_DIR) + FS + "extent-config.xml";
+    public static String PATH_REPORT = System.getProperty(USER_DIR) + FS + "TestReport" + FS;
     // REPORT
-    public static String REPORT_FILE_NAME = "Report.html";
-    public static String WINDOWS_PATH = System.getProperty(USER_DIR) + File.separator + "TestReport";
-    public static String WIN_REPORT_FILE_LOC = WINDOWS_PATH + File.separator + REPORT_FILE_NAME;
+    private static String REPORT_FILE_NAME = "Report.html";
+    public static String REPORT_FILE_LOC = PATH_REPORT + REPORT_FILE_NAME;
+
+    //For Report start time and End time format
+//    public static SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.ENGLISH);
 
 }
