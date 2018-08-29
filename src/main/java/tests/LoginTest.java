db1be872b3da3b49a855aaa7ed16420c1dd3d2d0
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-08-29 08:07:15 -0300

    retry briell

--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -16,7 +16,7 @@ public class LoginTest extends RegressionFunc {
     private HomePage homePage;
     private LoginPage loginPage;
 
-    @Test(priority = 1, testName = "Valid Login")
+    @Test(priority = 0, testName = "Valid Login", retryAnalyzer = Retry.class)
     public void validLoginTest() {
         homePage = new HomePage();
         loginPage = new LoginPage();
@@ -28,7 +28,7 @@ public class LoginTest extends RegressionFunc {
         testInstance.log(Status.PASS, "deu bom");
     }
 
-    @Test(priority = 1, testName = "Invalid Login - without password")
+    @Test(priority = 1, testName = "Invalid Login - without password", retryAnalyzer = Retry.class)
     public void invalidLoginTest_WrongPassword() {
 
         homePage = new HomePage();
@@ -40,7 +40,7 @@ public class LoginTest extends RegressionFunc {
         Assert.assertTrue(loginPage.isLoginFail());
     }
 
-    @Test(priority = 1, testName = "Invalid Login - wrong username")
+    @Test(priority = 1, testName = "Invalid Login - wrong username", retryAnalyzer = Retry.class)
     public void invalidLoginTest_WrongUsername() {
         homePage = new HomePage();
         loginPage = new LoginPage();
@@ -51,7 +51,7 @@ public class LoginTest extends RegressionFunc {
         Assert.assertTrue(loginPage.isLoginFail());
     }
 
-    @Test(priority = 1, testName = "Invalid Login - without username")
+    @Test(priority = 1, testName = "Invalid Login - without username", retryAnalyzer = Retry.class)
     public void invalidLoginTest_EmptyUsername() {
 
         homePage = new HomePage();
