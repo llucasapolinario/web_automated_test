da69faf4cc0b8cab4064d344c9e73ebdbf126444
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2018-10-12 15:28:26 -0300

    refactor OptionsManager

--- src/main/java/utils/OptionsManager.java
+++ src/main/java/utils/OptionsManager.java
@@ -4,16 +4,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static utils.Utils.getCurrentPlatform;
 
 
 class OptionsManager {
 
     ChromeOptions getChromeOptions() {
-        System.out.println("getChromeOptions");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1920,1080");
         options.addArguments("--start-maximized");
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
+        System.out.println("getChromeOptions end");
         return options;
     }
 
@@ -25,6 +29,14 @@ class OptionsManager {
         profile.setPreference("network.proxy.type", 0);
         options.setCapability(FirefoxDriver.PROFILE, profile);
         return options;
+    }
+
+    DesiredCapabilities getEdgeCapabilities() {
+        DesiredCapabilities capabilities = DesiredCapabilities.edge();
+        capabilities.setBrowserName("MicrosoftEdge");
+        capabilities.setPlatform(getCurrentPlatform());
+        capabilities.setCapability("acceptSslCerts", "true");
+        return capabilities;
     }
 
 }
