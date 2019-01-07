21930e201b9720609880efa9c33a8e042c6e4201
Lucas Apolinário Figueiredo <lucasapolinariofigueiredo@gmail.com>
2019-01-07 21:48:36 -0200

    db de volta

--- src/main/java/tests/Base/BaseTest.java
+++ src/main/java/tests/Base/BaseTest.java
@@ -17,24 +17,24 @@ public class BaseTest {
 
     @BeforeSuite
     public void cleanDB() throws SQLException {
-//        DBFactory dbFactory = new DBFactory();
-//
-//        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_text_table");
-//        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_project_table");
-//        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_category_table");
-//        dbFactory.DBRunQuery("INSERT INTO mantis_category_table (id, project_id, user_id, name, status) " +
-//                "VALUES (1, 0, 0, \"General\", 0);");
-//        System.out.println("db");
+        DBFactory dbFactory = new DBFactory();
+
+        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_text_table");
+        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_project_table");
+        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_category_table");
+        dbFactory.DBRunQuery("INSERT INTO mantis_category_table (id, project_id, user_id, name, status) " +
+                "VALUES (1, 0, 0, \"General\", 0);");
+        System.out.println("db");
 
     }
 
     @BeforeMethod
     public void setup() throws SQLException {
         System.out.println("setup");
-//
-//        DBFactory dbFactory = new DBFactory();
-//        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_table");
-//        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_history_table");
+
+        DBFactory dbFactory = new DBFactory();
+        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_table");
+        dbFactory.DBRunQuery("TRUNCATE TABLE mantis_bug_history_table");
         System.out.println("and setp");
         new BaseElement().goToPage(PropertyManager.getInstance().getURL());
     }
